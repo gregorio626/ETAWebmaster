621e10ff005e238ee8b9fd30c5c297f87dedc511
Nathaniel Jackson <nathanielchjackson@gmail.com>
2015-05-21 12:50:21 -0400

    Upload of the website - 5.21.15 at 11:51 AM

---
+++ tsa_chapter/tsa_chapter_js.js
@@ -0,0 +1,20 @@
+function changeImage() {
+
+
+        if (document.getElementById("crankster").src == "../images/gavel.gif") 
+
+        {
+
+            document.getElementById("crankster").src = "../images/christiansifuentes.jpg";
+
+        }
+
+        else 
+
+        {
+
+            document.getElementById("crankster").src = "../images/gavel.gif";
+
+        }
+
+    }
